722f06541d115063b4e39160155a3e4ca4985041
Mrrll <edgarmarrollo@gmail.com>
2024-12-15 15:55:35 +0100

    Incluimos jquery y jquery-ui al proyecto

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,5 @@
 import './bootstrap';
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from "bootstrap";
+window.bootstrap = bootstrap;
+
+import "jquery-ui/dist/jquery-ui";
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,9 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+import $ from "jquery";
+window.jQuery = window.$ = $;
+
+import * as Popper from "@popperjs/core";
+window.Popper = Popper;
